4def631c35c291d437d779787705117e207df86e
Kiran <kiranpuri532@gmail.com>
2023-07-27 19:41:24 +0530

    [Kiran]Add:\ Check file exist or not

---
+++ DailyPractice/DailyPractice/FileIO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPractice
+{
+    public class FileIO
+    {
+        string path = @"C:\Users\HP\Desktop\RFP-288\PracticeDaily\DailyPractice\DailyPractice\DailyPractice\PracticeFolder\Kiran.txt";
+
+        public void FileExist()
+        {
+            if(File.Exists(path))
+            {
+                Console.WriteLine("File does exist");
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+            }
+        }
+    }
+}
---
+++ DailyPractice/DailyPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                FileIO fileIO = new FileIO();
+                Console.WriteLine("1- Check File exist or not");
+                Console.WriteLine("Choose above options");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        fileIO.FileExist();
+                        break;
+                }
+            }
+        }
+    }
+}
