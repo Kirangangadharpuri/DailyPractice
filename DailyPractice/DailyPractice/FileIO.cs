1f8c7db933d8b20f415cce910a40b02ac86ecb8b
Kiran <kiranpuri532@gmail.com>
2023-07-27 20:07:27 +0530

    [Kiran]Add:\ Perform all file operations

--- DailyPractice/DailyPractice/FileIO.cs
+++ DailyPractice/DailyPractice/FileIO.cs
@@ -22,5 +22,53 @@ namespace DailyPractice
                 Console.WriteLine("File does not exist");
             }
         }
+
+        public void ReadAllLine()
+        {
+            string[] Lines= File.ReadAllLines(path);
+            foreach(string Line in Lines)
+            {
+                Console.WriteLine(Line);
+            }
+        }
+        public void ReadAllText()
+        {
+            string Line= File.ReadAllText(path);
+            Console.WriteLine(Line);
+        }
+
+        public void COpyFile()
+        {
+            string copyPath = @"C:\Users\HP\Desktop\RFP-288\PracticeDaily\DailyPractice\DailyPractice\DailyPractice\PracticeFolder\Puri.txt";
+            File.Copy(path, copyPath);
+            Console.WriteLine("File has been copied");
+        }
+
+        public void DeleteFile()
+        {
+            string copyPath = @"C:\Users\HP\Desktop\RFP-288\PracticeDaily\DailyPractice\DailyPractice\DailyPractice\PracticeFolder\Puri.txt";
+            File.Delete(copyPath);
+            Console.WriteLine("File has been deleted");
+        }
+        public void REadFile_UsingStreamReader()
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string reader =" ";
+                while((reader =sr.ReadLine()) !=null)
+                {
+                    Console.WriteLine(reader);
+                }
+            }
+        }
+        public void WriteFile_UsingStreamWriter()
+        {
+            using(StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("\nTo expertise my skills");
+                sw.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
     }
 }
--- DailyPractice/DailyPractice/Program.cs
+++ DailyPractice/DailyPractice/Program.cs
@@ -14,12 +14,36 @@ namespace DailyPractice
             {
                 FileIO fileIO = new FileIO();
                 Console.WriteLine("1- Check File exist or not");
+                Console.WriteLine("2- Read all lines");
+                Console.WriteLine("3- Read all text");
+                Console.WriteLine("4- Copy file");
+                Console.WriteLine("5-  Delete File ");
+                Console.WriteLine("6-read file using stream reader");
+                Console.WriteLine("7- Write text in file using StreamWriter");
                 Console.WriteLine("Choose above options");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
                     case 1:
                         fileIO.FileExist();
+                        break;
+                    case 2:
+                        fileIO.ReadAllLine();
+                        break;
+                    case 3:
+                        fileIO.ReadAllText();
+                        break;
+                    case 4:
+                        fileIO.COpyFile();
+                        break;
+                    case 5:
+                        fileIO.DeleteFile();
+                        break;
+                    case 6:
+                        fileIO.REadFile_UsingStreamReader();
+                        break;
+                    case 7:
+                        fileIO.WriteFile_UsingStreamWriter();
                         break;
                 }
             }
